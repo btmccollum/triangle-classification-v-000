b8a1495ffc04fc1cf67cef6355d79d6752e2ee7d
Brad McCollum <btmcco8@gmail.com>
2018-08-28 14:17:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
   # write code here
+  def kind(a, b, c)
+    if a == b && b ==c
+      :equilateral
+    elsif a!=b && b=c || a==b && b!=c || a==c && b!=c
+      :isosceles
+    elsif a != b != c
+      :scalene
+    end
+  end
+
+  class TriangleError < Standard Error
+
+  end
 end
