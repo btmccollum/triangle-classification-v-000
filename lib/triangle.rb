ea9999e34c3a74c6879572df449169b5fa691616
Brad McCollum <btmcco8@gmail.com>
2018-08-28 15:14:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if a == 0 || b == 0 || c == 0
       begin
-        raise TriangleError
+        raise Triangle::TriangleError
       rescue TriangleError => error
         puts error.message
       end
