1795918294bb47349b69bd71d73adf8f2724b0a2
Brad McCollum <btmcco8@gmail.com>
2018-08-28 15:47:37 +0000

    automatically backed up by learn

---
+++ lib/triangle_array.rb
@@ -0,0 +1,29 @@
+class Triangle
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    sides = [a, b, c].sort
+    if sides.include?(0) || (sides[0] + sides[1] <= sides[2])
+      begin
+        raise TriangleError
+      end
+    if sides.uniq.size == 3
+      :scalene
+    elsif side.uniq.size == 2
+      :isosceles
+    else
+      :equilateral
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle size provided."
+    end
+  end
+end
