45c656aab04af1c0ce120a18fdcc1304c06f9b32
Brad McCollum <btmcco8@gmail.com>
2018-08-28 15:35:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
       :equilateral
     elsif (a != b && b == c) || (a == b && b != c) || (a == c && b != c)
       :isosceles
-    elsif a != b && b != c
+    else
       :scalene
     end
   end
